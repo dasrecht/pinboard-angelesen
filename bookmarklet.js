1e98d98f35990ee9871e33079392635664f5d936
Bastian Widmer <b.widmer@dasrecht.net>
2016-01-06 01:45:15 +0100

    bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,26 @@
+d = new Date();
+yearNr = d.getFullYear();
+// Create a copy of this date object
+var target = new Date(d.valueOf());
+// ISO week date weeks start on monday
+// so correct the day number
+var dayNr = (d.getDay() + 6) % 7;
+// Set the target to the thursday of this week so the
+// target date is in the right year
+target.setDate(target.getDate() - dayNr + 3);
+// ISO 8601 states that week 1 is the week
+// with january 4th in it
+var jan4 = new Date(target.getFullYear(), 0, 4);
+// Number of days between target date and january 4th
+var dayDiff = (target - jan4) / 86400000;
+// Calculate week number: Week 1 (january 4th) plus the
+// number of weeks between target date and january 4th
+var weekNr = 1 + Math.ceil(dayDiff / 7);
+var weekNr = weekNr -1;
+
+if (document.getSelection) {
+    s = document.getSelection();
+} else {
+    s = '';
+};
+document.location = 'https://pinboard.in/add?next=same&url=' + encodeURIComponent(location.href) + '&description=' + encodeURIComponent(s) + '&title=' + encodeURIComponent(document.title) + '&tags=2blog 2blog-' + yearNr + '-' + weekNr
